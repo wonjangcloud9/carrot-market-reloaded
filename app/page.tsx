7821499b7eb7ce27649078523dcb56fe488dbbac
wonjangcloud9 <venuscwj@naver.com>
2024-04-01 21:47:06 +0900

    Lists and Animations

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,16 @@ import React from "react";
 export default function Home() {
   return (
     <main className="bg-slate-100 sm:bg-red-100 md:bg-green-100 h-screen flex justify-center items-center p-5 ">
-      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2 md:flex-row *:outline-none has-[invalid]:ring-red-100 ring ring-transparent transition-shadow">
-        <input
-          type="text"
-          className="w-full bg-gray-200 rounded-full h-10 pl-5  ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
-          required
-          placeholder="Search"
-        />
-        <span className="text-red-500 font-medium hidden peer-invalid:block">
-          Email is required
-        </span>
-        <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium  md:px-10 bg-black peer-required:bg-green-500">
-          Search
-        </button>
+      <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-4">
+        {["Nico", "Me", "You", "Yourself"].map((person, index) => (
+          <div key={index} className="flex items-center gap-5 ">
+            <div className="size-10 bg-blue-400 rounded-full" />
+            <span className="text-lg font-medium">{person}</span>
+            <div className="size-6 animate-bounce bg-red-500 text-white flex items-center justify-center rounded-full">
+              <span>{index}</span>
+            </div>
+          </div>
+        ))}
       </div>
     </main>
   );

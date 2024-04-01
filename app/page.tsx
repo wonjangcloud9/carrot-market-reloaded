a9bf02a074f1c89e403aa4ca81c4c7000036734f
wonjangcloud9 <venuscwj@naver.com>
2024-04-01 16:21:08 +0900

    Form Modifiers 중에서도 peer 개미뗬다...

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,14 @@ export default function Home() {
       <div className="bg-white w-full shadow-lg p-5 rounded-3xl max-w-screen-sm flex flex-col gap-2 md:flex-row">
         <input
           type="text"
-          className="w-full bg-gray-200 rounded-full h-10 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow"
+          className="w-full bg-gray-200 rounded-full h-10 pl-5 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow placeholder:drop-shadow invalid:focus:ring-red-500 peer"
+          required
           placeholder="Search"
         />
-        <button className="bg-black bg-opacity-50 text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10">
+        <span className="text-red-500 font-medium hidden peer-invalid:block">
+          Email is required
+        </span>
+        <button className="text-white py-2 rounded-full active:scale-90 transition-transform font-medium outline-none md:px-10 bg-black peer-required:bg-green-500">
           Search
         </button>
       </div>
